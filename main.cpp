8bc3f0640bf6c38f65bf11368e7f3bb9d917e54a
Nicolas Barragan <nico.barragan@gmail.com>
2021-02-28 23:40:25 +0000

    LeerArchivo

--- main.cpp
+++ main.cpp
@@ -1,12 +1,30 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
+void leerArchivo();
 
 int main() {
-  cout << "Hello World!\n";
+
+  leerArchivo();
 
 }
 
-void misHuevos(){
-     cout << "hola";
-     cout << "aaaaaa"; 
-}
+void leerArchivo(){
+      ifstream arch; 
+      string txt;
+
+     arch.open("prueba.fa", ios::in); //Se abre el archivo
+     if(arch.fail()){ //Condicion por si falla
+       cout << "No se puede abrir" << endl; 
+       exit(1);
+     }
+
+     while(!arch.eof()){
+       getline(arch, txt);
+       cout << txt << " " << endl;
+     }
+
+     arch.close();
+
+
+} 
