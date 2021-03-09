431b13eccb39fc7ca8a96d2f0c67c75d25c6929d
David <crisdavidgonzalezc@gmail.com>
2021-03-09 02:47:25 +0000

    Añadidos argc y argv

--- main.cpp
+++ main.cpp
@@ -1,66 +1,24 @@
 #include <iostream>
 #include <fstream>
-#include <conio.h>
 #include <string.h>
 #include <stdlib.h>
 
 using namespace std;
-void leerArchivo();
+void leerArchivo(string);
 
-int main() {
+int main(int argc, char *argv[]) {
 
-char comand[50]; 
-char *entrada;
-bool salir = false; 
+  if(strcmp(argv[1],"cargar")==0)
+    leerArchivo(argv[2]);
 
-  while(salir != true){
-    cout << "~$ " ;
-    cin.getline(comand, 50, '\n' );
-    entrada = strstr(comand, "cargar"); 
-     if(entrada != NULL){
-       cout << "cargando" << endl; 
-      }
-      else {
-          entrada = strstr(comand, "conteo"); 
-          if(entrada != NULL){
-          cout << "contando" << endl; 
-          } 
-          else {
-                entrada = strstr(comand, "listar");
-                if(entrada != NULL){
-                cout << "listando" << endl;
-                } 
-                else {
-                      entrada = strstr(comand, "histograma"); 
-                      if(entrada != NULL){
-                        cout << "histograma" << endl; 
-                      }   
-                      else {
-                        entrada = strstr(comand, "salir"); 
-                        if(entrada != NULL){
-                          cout << "saliendo" << endl; 
-                          salir = true; 
-                        }
-                      }
-          }  
-     }
-
-    }
-      
-    system("pause");      
-    system("cls"); 
-  }
-
-  cout << endl; 
-  leerArchivo();
 
 }
 
-void leerArchivo(){
+void leerArchivo(string archivo){
       ifstream arch; 
       string txt;
 
-     arch.open("prueba.fa", ios::in); //Se abre el archivo
+     arch.open(archivo, ios::in); //Se abre el archivo
      if(arch.fail()){ //Condicion por si falla
        cout << "No se puede abrir" << endl; 
        exit(1);
