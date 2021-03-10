774377b7cf3fff82ec560ad27bae014f78c27dd5
Nicolas Barragan <nbarragans@javeriana.edu.co>
2021-03-10 18:27:02 -0500

    Unos cambios mks

--- main.cpp
+++ main.cpp
@@ -1,16 +1,76 @@
 #include <iostream>
 #include <fstream>
+#include <conio.h>
 #include <string.h>
 #include <stdlib.h>
 
 using namespace std;
-void leerArchivo(string);
+void leerArchivo();
 
-int main(int argc, char *argv[]) {
+int main() {
 
-  if(strcmp(argv[1],"cargar")==0)
-    leerArchivo(argv[2]);
+char comand[50]; 
+char *entrada;
+bool salir = false; 
 
+  while(salir != true){
+    cout << "~$ " ;
+    cin.getline(comand, 50, '\n' );
+    entrada = strstr(comand, "cargar"); 
+     if(entrada != NULL){
+       cout << "cargando" << endl; 
+      }
+      else {
+          entrada = strstr(comand, "conteo"); 
+          if(entrada != NULL){
+          cout << "contando" << endl; 
+          } 
+          else {
+                entrada = strstr(comand, "listar");
+                if(entrada != NULL){
+                cout << "listando" << endl;
+                } 
+                else {
+                      entrada = strstr(comand, "histograma"); 
+                      if(entrada != NULL){
+                        cout << "histograma" << endl; 
+                      }   
+                      else {
+                            entrada = strstr(comand, "es_subsecuencia"); 
+                            if(entrada != NULL){
+                             cout << "Buscando subsecuencia" << endl;
+                            }
+                            else {
+                                  entrada = strstr(comand, "enmascarar"); 
+                                  if(entrada != NULL){
+                                    cout << "Enmascarando " << endl; 
+                                  }
+                                  else {
+                                      entrada = strstr(comand, "guardar");
+                                      if(entrada != NULL){
+                                        cout << "Guardando archivo" << endl; 
+                                      }
+                                      else {
+                                          entrada = strstr(comand, "salir"); 
+                                          if(entrada != NULL){
+                                            cout << "Saliendo" << endl; 
+                                            salir = true; 
+                                          }
+                                          }
+                                        }
+                                    }     
+                              } 
+                        }  
+                }
+
+          }
+      
+    system("pause");      
+    system("cls"); 
+  }
+
+  cout << endl; 
+  leerArchivo();
 
 }
 
